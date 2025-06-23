9a083583218204a1a9f73f61f35da7cd6715cdb5
mapfactory121 <mapfactory363@gmail.com>
2025-06-24 00:58:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-// Modal logic
-const orderBtn = document.getElementById('orderBtn');
-const orderModal = document.getElementById('orderModal');
-const closeModal = document.getElementById('closeModal');
-const orderForm = document.getElementById('orderForm');
-const formMsg = document.getElementById('formMsg');
-
-// Open modal
-orderBtn.onclick = function() {
-  orderModal.style.display = 'flex';
-  formMsg.textContent = '';
-  orderForm.reset();
-};
-
-// Close modal
-closeModal.onclick = function() {
-  orderModal.style.display = 'none';
-};
-
-// Close modal when clicking outside content
-window.onclick = function(event) {
-  if (event.target === orderModal) {
-    orderModal.style.display = 'none';
-  }
-};
-
-// Optional: Show a message after form submit (Formspree will redirect by default)
-orderForm.onsubmit = function(e) {
-  formMsg.textContent = 'Sending...';
-};
+// Sound effect for button hover
+function playSound() {
+  const sound = new Audio("https://cdn.pixabay.com/download/audio/2023/03/23/audio_6c01fe75ba.mp3?filename=ui-click-136746.mp3");
+  sound.volume = 0.3;
+  sound.play();
+}
